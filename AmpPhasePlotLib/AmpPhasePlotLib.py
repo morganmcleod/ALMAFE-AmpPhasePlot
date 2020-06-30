7a561f1a02085911e6d4d94d7d387d8e8851fe7f
Morgan McLeod <mmcleod@nrao.edu>
2020-06-30 16:52:12 -0400

    Getting started

--- AmpPhasePlotLib/AmpPhasePlotLib.py
+++ AmpPhasePlotLib/AmpPhasePlotLib.py
@@ -1,18 +1,24 @@
 '''
-Created on May 20, 2020
-
-@author: mmcleod
 '''
+from AmpPhaseDataLib import TimeSeriesAPI
+import plotly.graph_objects as go
 
-class AmpPhasePlotLib(object):
+class PlotApi(object):
     '''
     classdocs
     '''
 
-
-    def __init__(self, params):
+    def __init__(self, timeSeriesAPI = None):
         '''
         Constructor
         '''
+        self.timeSeriesAPI = timeSeriesAPI if timeSeriesAPI else TimeSeriesAPI.TimeSeriesAPI()
         
+    def plotTimeSeries(self, timeSeriesId):
+        ts = self.timeSeriesAPI
+        if not ts.retrieveTimeSeries(timeSeriesId):
+            return None
+    
+        fig = go.Figure(go.Scatter(x = ts.timeStamps, y = ts.dataSeries, mode = 'lines'))
+        fig.show()
     
