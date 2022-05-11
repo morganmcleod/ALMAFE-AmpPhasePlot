f94b1380dd629b14756628018abd575cddc5d4a1
Morgan McLeod <mmcleod@nrao.edu>
2022-05-11 11:27:37 -0400

    Fix edge case in testing Amplitude Stabilty spec Generalize tests/Integration/IFProcAmplitudeStability

--- tests/Integration/IFPocAmpStability.py
+++ tests/Integration/IFPocAmpStability.py
@@ -1,32 +1,38 @@
+import os
+os.chdir('L:\python\ALMAFE-AmpPhasePlot')
+
 from AmpPhaseDataLib.Constants import DataSource, Units, PlotEl, SpecLines
 from AmpPhaseDataLib.LegacyImport import importTimeSeriesE4418B
 from AmpPhaseDataLib import TimeSeriesAPI
 from AmpPhasePlotLib import PlotAPI
-import os
-
 tsa = TimeSeriesAPI.TimeSeriesAPI()
 plt = PlotAPI.PlotAPI()
 
-os.chdir('L:\python\ALMAFE-AmpPhasePlot')
+myPath = './SampleData-local/IFPROCV3/2022-05-06'
 
-ds0 = importTimeSeriesE4418B('./SampleData-local/IFPROCV3/2022-04-29/IFPv3-IF0.txt', notes = 'bench test', tau0Seconds = 0.05, importUnits = Units.WATTS)
-ds1 = importTimeSeriesE4418B('./SampleData-local/IFPROCV3/2022-04-29/IFPv3-IF1.txt', notes = 'bench test', tau0Seconds = 0.05, importUnits = Units.WATTS)
-ds2 = importTimeSeriesE4418B('./SampleData-local/IFPROCV3/2022-04-29/IFPv3-IF2.txt', notes = 'bench test', tau0Seconds = 0.05, importUnits = Units.WATTS)
-ds3 = importTimeSeriesE4418B('./SampleData-local/IFPROCV3/2022-04-29/IFPv3-IF3.txt', notes = 'bench test', tau0Seconds = 0.05, importUnits = Units.WATTS)
+# https://stackoverflow.com/questions/3207219/how-do-i-list-all-files-of-a-directory
+(_, _, filenames) = next(os.walk(myPath))
 
-tsa.setDataSource(ds0, DataSource.TEST_SYSTEM, "Andrew Smith bench")
-tsa.setDataSource(ds1, DataSource.TEST_SYSTEM, "Andrew Smith bench")
-tsa.setDataSource(ds2, DataSource.TEST_SYSTEM, "Andrew Smith bench")
-tsa.setDataSource(ds3, DataSource.TEST_SYSTEM, "Andrew Smith bench")
-
-tsa.setDataSource(ds0, DataSource.SUBSYSTEM, "IF0")
-tsa.setDataSource(ds1, DataSource.SUBSYSTEM, "IF1")
-tsa.setDataSource(ds2, DataSource.SUBSYSTEM, "IF2")
-tsa.setDataSource(ds3, DataSource.SUBSYSTEM, "IF3")
-
-plotEls = { PlotEl.TITLE : "IF Processor V3 Amplitude Stability", 
-            PlotEl.SPEC_LINE1 : SpecLines.IFPROC_AMP_STABILITY1, 
-            PlotEl.SPEC_LINE2 : SpecLines.IFPROC_AMP_STABILITY2
-          } 
-
-plt.plotAmplitudeStability([ds0,ds1], plotElements = plotEls, outputName = '.\\SampleData-local\\IFPROCV3\\2022-04-29\\StabilityIF0IF1.png', show = True)    
+notes = notes = 'bench test'
+dataSource = "Andrew Smith bench"
+tau0Seconds = 0.05
+timePlotEls = { 
+    PlotEl.TITLE : "IF Processor V3 Amplitude Stability"
+}
+spectrumPlotEls = { 
+    PlotEl.TITLE : "IF Processor V3 Amplitude Stability", 
+    PlotEl.SPEC_LINE1 : SpecLines.IFPROC_AMP_STABILITY1, 
+    PlotEl.SPEC_LINE2 : SpecLines.IFPROC_AMP_STABILITY2
+}
+ 
+tsIds = []
+for name in filenames:
+    try:
+        tsId = importTimeSeriesE4418B(myPath + "/" + name, notes, tau0Seconds = tau0Seconds, importUnits = Units.WATTS)
+        tsa.setDataSource(tsId, DataSource.TEST_SYSTEM, dataSource)
+        plt.plotTimeSeries(tsId, timePlotEls, outputName = myPath + "/" + str(tsId) + ".png", show = True)
+        tsIds.append(tsId)
+    except:
+        pass
+         
+plt.plotAmplitudeStability(tsIds, plotElements = spectrumPlotEls, outputName = myPath + "/stability.png", show = True)    
