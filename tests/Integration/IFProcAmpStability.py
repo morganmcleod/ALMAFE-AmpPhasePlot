68d2716663efe4974b7ced72a4b01199d91a9793
Morgan McLeod <mmcleod@nrao.edu>
2022-05-27 10:41:30 -0400

    IFProcAmpStability:  one FFT plot per input

--- tests/Integration/IFProcAmpStability.py
+++ tests/Integration/IFProcAmpStability.py
@@ -8,31 +8,27 @@ from AmpPhasePlotLib import PlotAPI
 tsa = TimeSeriesAPI.TimeSeriesAPI()
 plt = PlotAPI.PlotAPI()
 
-myPath = './SampleData-local/IFPROCV3/2022-05-06'
+myPath = './SampleData-local/IFPROCV3/2022-05-27/2'
 
 # https://stackoverflow.com/questions/3207219/how-do-i-list-all-files-of-a-directory
 (_, _, filenames) = next(os.walk(myPath))
 
 notes = notes = 'bench test'
-dataSource = "Andrew Smith bench"
+dataSource = "Cart in chamber"
 tau0Seconds = 0.05
 timePlotEls = { 
     PlotEl.TITLE : "IF Processor V3 Amplitude Stability"
 }
 spectrumPlotEls = { 
-    PlotEl.TITLE : "IF Processor V3 Amplitude Stability", 
+    PlotEl.TITLE : "IF Processor V3 Amplitude Stability, internal power sensor", 
     PlotEl.SPEC_LINE1 : SpecLines.IFPROC_AMP_STABILITY1, 
     PlotEl.SPEC_LINE2 : SpecLines.IFPROC_AMP_STABILITY2
 }
  
-tsIds = []
 for name in filenames:
-    try:
+    (_, ext) = os.path.splitext(name)
+    if ext.lower() == ".txt":
         tsId = importTimeSeriesE4418B(myPath + "/" + name, notes, tau0Seconds = tau0Seconds, importUnits = Units.WATTS)
         tsa.setDataSource(tsId, DataSource.TEST_SYSTEM, dataSource)
         plt.plotTimeSeries(tsId, timePlotEls, outputName = myPath + "/" + str(tsId) + ".png", show = True)
-        tsIds.append(tsId)
-    except:
-        pass
-         
-plt.plotAmplitudeStability(tsIds, plotElements = spectrumPlotEls, outputName = myPath + "/stability.png", show = True)    
+        plt.plotAmplitudeStability([tsId], plotElements = spectrumPlotEls, outputName = myPath + "/" + str(tsId) + "-stability.png", show = True)    
