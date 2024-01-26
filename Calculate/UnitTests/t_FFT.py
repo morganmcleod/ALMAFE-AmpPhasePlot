2138b3d9689491090ffb2525a1c24669ce392256
morganmcleod <mmcleod@nrao.edu>
2024-01-26 10:24:30 -0500

    Calculate/UnitTests/t_FFT:  store plots in SampleData/t_FFT

--- Calculate/UnitTests/t_FFT.py
+++ Calculate/UnitTests/t_FFT.py
@@ -3,8 +3,12 @@ from AmpPhaseDataLib import TimeSeriesAPI
 from AmpPhasePlotLib import PlotAPI
 import numpy as np
 from datetime import datetime
+import os
 
 def makeFFTSamples():
+
+    outputPath = './SampleData/t_FFT'
+
     tsAPI = TimeSeriesAPI.TimeSeriesAPI()
     plotAPI = PlotAPI.PlotAPI()
 
@@ -24,63 +28,63 @@ def makeFFTSamples():
     tsId = tsAPI.insertTimeSeries(s, tau0Seconds = interval, startTime = now)
     tsAPI.setDataSource(tsId, DataSource.DATA_SOURCE, "40 Hz sin at amplitude 1")
     plotAPI.plotTimeSeries(tsId, dict(tsEls), show = False)
-    plotAPI.plotSpectrum(tsId, dict(fftEls), outputName = "1 - 40.png")
+    plotAPI.plotSpectrum(tsId, dict(fftEls), outputName = os.path.join(outputPath, "1 - 40.png"))
     tsAPI.deleteTimeSeries(tsId)
  
     s = (0.5 * np.sin(40 * 2 * np.pi * t)).tolist()
     tsId = tsAPI.insertTimeSeries(s, tau0Seconds = interval, startTime = now)
     tsAPI.setDataSource(tsId, DataSource.DATA_SOURCE, "40 Hz sin at amplitude 0.5")
     plotAPI.plotTimeSeries(tsId, dict(tsEls), show = False)
-    plotAPI.plotSpectrum(tsId, dict(fftEls), outputName = "2 - 40 at 0.5.png")
+    plotAPI.plotSpectrum(tsId, dict(fftEls), outputName = os.path.join(outputPath, "2 - 40 at 0.5.png"))
     tsAPI.deleteTimeSeries(tsId)
  
     s = (np.sin(40 * 2 * np.pi * t) ** 2).tolist()
     tsId = tsAPI.insertTimeSeries(s, tau0Seconds = interval, startTime = now)
     tsAPI.setDataSource(tsId, DataSource.DATA_SOURCE, "40 Hz sin^2 at amplitude 1")
     plotAPI.plotTimeSeries(tsId, dict(tsEls), show = False)
-    plotAPI.plotSpectrum(tsId, dict(fftEls), outputName = "3 - sin^2.png")
+    plotAPI.plotSpectrum(tsId, dict(fftEls), outputName = os.path.join(outputPath, "3 - sin^2.png"))
     tsAPI.deleteTimeSeries(tsId)
  
     s = (0.5 * np.sin(40 * 2 * np.pi * t) ** 2).tolist()
     tsId = tsAPI.insertTimeSeries(s, tau0Seconds = interval, startTime = now)
     tsAPI.setDataSource(tsId, DataSource.DATA_SOURCE, "40 Hz sin^2 at amplitude 0.5")
     plotAPI.plotTimeSeries(tsId, dict(tsEls), show = False)
-    plotAPI.plotSpectrum(tsId, dict(fftEls), outputName = "4 - sin^2 at 0.5.png")
+    plotAPI.plotSpectrum(tsId, dict(fftEls), outputName = os.path.join(outputPath, "4 - sin^2 at 0.5.png"))
     tsAPI.deleteTimeSeries(tsId)
     
     s = (np.sin(40 * 2 * np.pi * t) + 0.5 * np.sin(90 * 2 * np.pi * t)).tolist()
     tsId = tsAPI.insertTimeSeries(s, tau0Seconds = interval, startTime = now)
     tsAPI.setDataSource(tsId, DataSource.DATA_SOURCE, "40 Hz sin at amplitude 1 + 90 Hz at 0.5")
     plotAPI.plotTimeSeries(tsId, dict(tsEls), show = False)
-    plotAPI.plotSpectrum(tsId, dict(fftEls), outputName = "5 - 40+90.png")
+    plotAPI.plotSpectrum(tsId, dict(fftEls), outputName = os.path.join(outputPath, "5 - 40+90.png"))
     tsAPI.deleteTimeSeries(tsId)
  
     s = (0.5 * np.sin(40 * 2 * np.pi * t) + 0.25 * np.sin(90 * 2 * np.pi * t)).tolist()
     tsId = tsAPI.insertTimeSeries(s, tau0Seconds = interval, startTime = now)
     tsAPI.setDataSource(tsId, DataSource.DATA_SOURCE, "40 Hz sin at amplitude 0.5 + 90 Hz at 0.25")
     plotAPI.plotTimeSeries(tsId, dict(tsEls), show = False)
-    plotAPI.plotSpectrum(tsId, dict(fftEls), outputName = "6 - 40+90 at 0.5.png")
+    plotAPI.plotSpectrum(tsId, dict(fftEls), outputName = os.path.join(outputPath, "6 - 40+90 at 0.5.png"))
     tsAPI.deleteTimeSeries(tsId)
 
     s = (np.sin(40 * 2 * np.pi * t) + np.sin(43 * 2 * np.pi * t)).tolist()
     tsId = tsAPI.insertTimeSeries(s, tau0Seconds = interval, startTime = now)
     tsAPI.setDataSource(tsId, DataSource.DATA_SOURCE, "40 + 43 Hz sin - resolvable in 1 Hz bins")
     plotAPI.plotTimeSeries(tsId, dict(tsEls), show = False)
-    plotAPI.plotSpectrum(tsId, dict(fftEls), outputName = "7 - 40+43 resolvable.png")
+    plotAPI.plotSpectrum(tsId, dict(fftEls), outputName = os.path.join(outputPath, "7 - 40+43 resolvable.png"))
     tsAPI.deleteTimeSeries(tsId)
     
     s = (np.sin(40 * 2 * np.pi * t) + np.sin(40.5 * 2 * np.pi * t)).tolist()
     tsId = tsAPI.insertTimeSeries(s, tau0Seconds = interval, startTime = now)
     tsAPI.setDataSource(tsId, DataSource.DATA_SOURCE, "40 + 40.5 Hz sin - unresolvable in 1 Hz bins")
     plotAPI.plotTimeSeries(tsId, dict(tsEls), show = False)
-    plotAPI.plotSpectrum(tsId, dict(fftEls), outputName = "8 - 40+40.5 unresolvable.png")
+    plotAPI.plotSpectrum(tsId, dict(fftEls), outputName = os.path.join(outputPath, "8 - 40+40.5 unresolvable.png"))
     tsAPI.deleteTimeSeries(tsId)
     
     s = np.random.normal(0, 1, size=samples).tolist()
     tsId = tsAPI.insertTimeSeries(s, tau0Seconds = interval, startTime = now)
     tsAPI.setDataSource(tsId, DataSource.DATA_SOURCE, "normal(mean=0, stddev=1)")
-    plotAPI.plotTimeSeries(tsId, dict(tsEls), outputName = "9 - normal noise.png")
-    plotAPI.plotSpectrum(tsId, dict(fftEls), outputName = "9 - normal noise FFT.png")
+    plotAPI.plotTimeSeries(tsId, dict(tsEls), outputName = os.path.join(outputPath, "9 - normal noise.png"))
+    plotAPI.plotSpectrum(tsId, dict(fftEls), outputName = os.path.join(outputPath, "9 - normal noise FFT.png"))
     tsAPI.deleteTimeSeries(tsId)
     
     samples = 1000
@@ -92,6 +96,6 @@ def makeFFTSamples():
     tsId = tsAPI.insertTimeSeries(s, tau0Seconds = interval, startTime = now)
     tsAPI.setDataSource(tsId, DataSource.DATA_SOURCE, "250 Hz sin at amplitude 1")
     plotAPI.plotTimeSeries(tsId, dict(tsEls), show = False)
-    plotAPI.plotSpectrum(tsId, dict(fftEls), outputName = "10 - 250.png")
+    plotAPI.plotSpectrum(tsId, dict(fftEls), outputName = os.path.join(outputPath, "10 - 250.png"))
     tsAPI.deleteTimeSeries(tsId)
  
